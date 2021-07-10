df5d160c01403ed4d1b9200f93486ec276deebf1
TheKlepo75 <antonio.klepo1@gmail.com>
2021-07-10 20:00:26 +0200

    commit

---
+++ js.js
@@ -0,0 +1,25 @@
+/*
+const request = new XMLHttpRequest(); 
+//request.open(`GET`,`https://jsonplaceholder.typicode.com/users`);
+request.open('GET',`http://192.168.5.21:3000/konzola`);
+request.send();
+request.onload = ()=> {
+  console.log(request);
+  if(request.status === 200){
+    console.log(JSON.parse(request.response));
+    //document.getElementById('paragraph').innerHTML = JSON.parse(request.response);
+  } else {
+    console.log(request);
+    console.log(`error ${request.status}`);
+  }
+}
+*/
+
+var konzola = document.getElementById("upisKonzole").innerHTML = '001';
+var brojIgre = document.querySelector("idIgre");
+var pozicija = document.querySelector("upisPozicija");
+var ulog = document.querySelector("upisUloga");
+
+var ispis
+
+var kredit = document.getElementById("kreditCifra").innerHTML = 1000;
